ad919eef1a39052d1324197ba930f0f2fae74423
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-07-22 23:24:27 +0430

    Session 5 ended.

--- Session 5.sql
+++ Session 5.sql
@@ -192,11 +192,27 @@ WHERE o.EmployeeID <> 9
  GROUP BY o.EmployeeID;
  GO
 
- /*
+-- All customer with more than 20 orders
 
- HERE
+ SELECT * From dbo.Orders
+ SELECT * From dbo.Customers
 
- */
+ SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num
+ FROm dbo.Orders
+ GROUP BY CustomerID
+	HAVING COUNT(OrderID) > 20;
+GO
+
+-- More than 70 orders and orders not by employee 8
+
+SELECT 
+OrderID
+FROM dbo.Orders
+	WHERE EmployeeID <> 9
+GROUP BY OrderID
+	HAVING COUNT(orderID) > 70;
 
  -- 5 customers with most orders 
 
@@ -284,3 +300,215 @@ GROUP BY ALL EmployeeID
 ORDER BY EmployeeID;
 GO
 
+
+/*
+Roll up (A, B, C)
+(A, B, C)
+(A, B)
+(A)
+()
+*/
+
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY CustomerID;
+GO
+
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY CustomerID WITH  ROLLUP;
+GO
+
+-- Best practice for above query (Write rollup after GROUP BY)
+
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY ROLLUP (CustomerID);
+GO
+
+
+-- Shoiwng customers and their order count and total sum for groups
+
+--TIP: In GROUP BY queries all columnss in select should be in the GROUP BY section except aggregate functions.
+SELECT
+	EmployeeID,
+	YEAR(OrderDate) AS OrderYear,
+	MONTH(OrderDate) OrderMonth,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+	WHERE EmployeeID IN (1,2)
+GROUP BY ROLLUP (EmployeeID,YEAR(OrderDate),MONTH(OrderDate));
+GO
+
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num,
+	GROUPING (CustomerID) AS GroupingCustomerID
+FROM dbo.Orders
+GROUP BY ROLLUP (CustomerID);
+GO
+
+SELECT
+	EmployeeID,
+	YEAR(OrderDate) AS OrderYear,
+	MONTH(OrderDate) AS OrderMonth,
+	COUNT(OrderID) AS Num,
+	GROUPING (EmployeeID) AS GROUPING_EMPLOYEEID,
+	GROUPING (YEAR(OrderDate)) AS GROUPING_YEAR,
+	GROUPING (MONTH(OrderDate)) AS GROUPING_MONTH
+FROM dbo.Orders
+GROUP BY ROLLUP (EmployeeID,YEAR(OrderDate),MONTH(OrderDate))
+
+/*
+Cube is 2 to the power of N
+CUBE (A, B, C)
+(A, B, C)
+(A, B)
+(A, C)
+(B, C)
+(A)
+(B)
+(C)
+()
+*/
+
+
+SELECT
+CustomerID,
+COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY CustomerID WITH CUBE;
+GO
+
+
+SELECT
+CustomerID,
+COUNT(OrderID) AS Num
+FROM dbo.Orders
+GROUP BY CUBE (CustomerID);
+GO
+
+SELECT
+	EmployeeID,
+	YEAR(OrderDate) AS OrderYear,
+	MONTH(OrderDate) OrderMonth,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+	WHERE EmployeeID IN (1,2)
+GROUP BY CUBE (EmployeeID,YEAR(OrderDate),MONTH(OrderDate))
+ORDER BY EmployeeID;
+Go
+
+
+SELECT
+	CustomerID,
+	COUNT(OrderID) AS Num,
+	GROUPING (CustomerID) AS GroupingCustomerID
+FROM dbo.Orders
+GROUP BY CUBE (CustomerID);
+GO
+
+SELECT
+	EmployeeID,
+	YEAR(OrderDate) AS OrderYear,
+	MONTH(OrderDate) AS OrderMonth,
+	COUNT(OrderID) AS Num,
+	GROUPING (EmployeeID) AS GROUPING_EMPLOYEEID,
+	GROUPING (YEAR(OrderDate)) AS GROUPING_YEAR,
+	GROUPING (MONTH(OrderDate)) AS GROUPING_MONTH
+FROM dbo.Orders
+GROUP BY CUBE (EmployeeID,YEAR(OrderDate),MONTH(OrderDate))
+
+
+-- GROUPING SET
+-- TIP: All SELECT fields should  be in GROUPING SETS in some way, except AGGREGATE COLUMNS.
+SELECT
+	EmployeeID, 
+	CustomerID,
+	YEAR(OrderDate) AS OrderYear,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+	WHERE CustomerID = 1 OR CustomerID = 2
+GROUP BY GROUPING SETS 
+	(
+		(EmployeeID, CustomerID),
+		(EmployeeID, YEAR(OrderDate)),
+		(CustomerID, YEAR(OrderDate)),
+		()
+	);
+GO
+
+SELECT
+	EmployeeID, 
+	CustomerID,
+	YEAR(OrderDate) AS OrderYear,
+	COUNT(OrderID) AS Num
+FROM dbo.Orders
+	WHERE CustomerID = 1 OR CustomerID = 2
+GROUP BY GROUPING SETS 
+	(
+		(EmployeeID, CustomerID),
+		(EmployeeID, YEAR(OrderDate)),
+		(CustomerID, YEAR(OrderDate)),
+		()
+	)
+ORDER BY 
+	CASE
+		WHEN YEAR(OrderDate) IS NULL THEN 1
+		WHEN EmployeeID IS NULL THEN 2
+		WHEN CustomerID IS NULL THEN 3
+	END;
+GO
+
+/*
+ GROUPING_ID
+ Begins with
+ 2^0 from right which means 1
+ 2^1 = 2
+ 2^2 = 4
+ it means the equal field isn't counted.
+In	CASE GROUPING_ID(EmployeeID,CustomerID, YEAR(OrderDate)) 
+  2^0 = 1 is  YEAR(OrderDate) which means  YEAR(OrderDate) isn't calculated so you can use the name 'customer and employee'
+  2^1 = 2 is  CustomerID which means  CustomerID isn't calculated so you can use the name 'employee and orderDate'
+  2^2 = 4 is  EmployeeID which means  EmployeeID isn't calculated so you can use the name 'customer and orderDate'
+
+ */
+SELECT
+	EmployeeID, 
+	CustomerID,
+	YEAR(OrderDate) AS OrderYear,
+	GROUPING_ID(EmployeeID,CustomerID, YEAR(OrderDate)) AS GROUPING_ID_FIELD 
+FROM dbo.Orders
+	WHERE CustomerID = 1 OR CustomerID = 2
+GROUP BY GROUPING SETS 
+	(
+		(EmployeeID, CustomerID),
+		(EmployeeID, YEAR(OrderDate)),
+		(CustomerID, YEAR(OrderDate)))
+GO
+
+
+SELECT
+	EmployeeID, 
+	CustomerID,
+	YEAR(OrderDate) AS OrderYear,
+	COUNT(orderid)AS Num,
+	CASE GROUPING_ID(EmployeeID,CustomerID, YEAR(OrderDate)) 
+		WHEN 4 THEN N'Customer and year'
+		WHEN 2 THEN N'Employee and year'
+		WHEN 1 THEN N'Employee and customer'
+	END AS N'Grouping base on'
+FROM dbo.Orders
+	WHERE CustomerID = 1 OR CustomerID = 2
+GROUP BY GROUPING SETS 
+	(
+		(EmployeeID, CustomerID),
+		(EmployeeID, YEAR(OrderDate)),
+		(CustomerID, YEAR(OrderDate)));
+GO
