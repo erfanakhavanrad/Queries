b668ebb76be20802246adea8151fcab21f8c94c3
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-07-24 00:14:55 +0430

    Session 6 modified.

--- Session 6.sql
+++ Session 6.sql
@@ -33,11 +33,80 @@ GO
 
 SELECT * from Employees
 
+-- Issues with self join
+-- Self pairs:	 Tim Brown	Tim Brown
+-- Mirrored pairs:	 Tim Brown Adele Kreg	Adele Kreg	Tim Brown
+SELECT
+	e1.FirstName,
+	e1.LastName,
+	e2.FirstName,
+	e2.LastName 
+FROM Employees AS e1
+CROSS JOIN Employees AS e2;
+GO
+
+-- Inner Join
 
 SELECT
-	FirstName,
-	LastName 
-FROM Employees
+	e.FirstName, e.LastName,
+	o.OrderID
+FROM dbo.Employees AS e
+INNER JOIN dbo.Orders AS o
+	ON o.EmployeeID  = e.EmployeeID;
+GO
+
+SELECT * FROM dbo.Orders
+SELECT * FROM dbo.Employees
+
+SELECT
+	e.FirstName, e.LastName,
+	o.OrderID
+FROM dbo.Employees AS e
+INNER JOIN dbo.Orders AS o
+	ON o.EmployeeID  = e.EmployeeID
+	WHERE e.LastName NOT LIKE N'[ا]%'
+ORDER BY e.LastName;
+GO
 
 
+SELECT
+	e.FirstName, e.LastName,
+	o.OrderID
+FROM dbo.Employees AS e
+INNER JOIN dbo.Orders AS o
+	ON o.EmployeeID  = e.EmployeeID
+	WHERE e.LastName LIKE N'[ب-ی]%'
+ORDER BY e.LastName;
+GO
 
+
+SELECT
+	e.FirstName, e.LastName,
+	o.OrderID
+FROM dbo.Employees AS e
+INNER JOIN dbo.Orders AS o
+	ON o.EmployeeID  = e.EmployeeID
+	WHERE e.LastName LIKE N'[^ا]%'
+ORDER BY e.LastName;
+GO
+
+SELECT 
+	COUNT(o.OrderID) AS Num,
+	c.City
+FROM dbo.Customers AS c
+INNER JOIN dbo.Orders AS o
+	ON o.CustomerID = c.CustomerID
+GROUP BY c.City
+	HAVING COUNT(o.OrderID) > 50;
+GO
+
+
+SELECT 
+	TOP (1) WITH TIES COUNT(o.OrderID) AS Num,
+	c.City
+FROM dbo.Customers AS c
+INNER JOIN dbo.Orders AS o
+	ON o.CustomerID = c.CustomerID
+GROUP BY c.City
+ORDER BY Num
+GO
