75d1a47a54da814627cdaa43d2323808932b2c2b
Erfan <erfanakhavanrad@outlook.com>
2022-07-23 18:01:15 +0430

    sefsef

---
+++ Session 6.sql
@@ -0,0 +1,43 @@
+﻿use SampleDb
+
+/*
+Session 6
+*/
+
+
+-- CROSS JOIN
+SELECT 
+	CustomerID,
+	EmployeeID
+FROM dbo.Customers
+CROSS JOIN dbo.Employees;
+GO
+
+SELECT
+c.CustomerID, o.EmployeeID
+FROM dbo.Customers AS c
+CROSS JOIN dbo.Orders AS o;
+GO
+
+SELECT * FROM Customers
+SELECT * FROM Orders
+
+SELECT
+	CustomerID, EmployeeID
+FROM dbo.Customers
+CROSS JOIN dbo.Employees
+	WHERE CustomerID > 90;
+GO
+
+-- SELF JOIN
+
+SELECT * from Employees
+
+
+SELECT
+	FirstName,
+	LastName 
+FROM Employees
+
+
+
