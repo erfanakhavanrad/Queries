8a5069b10b7c93b048e6336584ce11f044faa9e3
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-08-09 23:47:50 +0430

    Session 7 updated.

--- Session 7.sql
+++ Session 7.sql
@@ -121,7 +121,7 @@ SELECT
 FROM dbo.Orders
 GROUP BY EmployeeID;
 GO
-
+-- Self-Contained Single-valued subquery
 /*
 کارمندانی که نام خانوداگی انها با پ شروع میشود
 */
@@ -146,10 +146,88 @@ GO
 تمامی سفارشات ثبت شده توسط کارمندانی که نام خانوداگی انها با پ شروع میشود
 با استفاده از Subquery
 */
-SELECT 
-	e.EmployeeID, o.OrderID
-FROM dbo.Employees AS e
-JOIN dbo.orders AS o
-	ON e.EmployeeID = o.EmployeeID
-	WHERE lastname LIKE N'پ%';
+SELECT
+	o.EmployeeID, o.OrderID
+FROM dbo.Orders AS o
+	WHERE o.EmployeeID =(SELECT EmployeeID FROM dbo.Employees WHERE lastname LIKE N'پ%');
 GO
+
+
+-- Self-Contained Multi-valued subquery
+/*
+کارمندانی که نام خانوداگی انها ب ت آغاز میشود.
+*/
+SELECT
+	*
+FROM dbo.Employees
+	WHERE lastname LIKE N'ت%';
+GO
+/*
+تمامی سفارشات ثبت شده توسط کارمندانی که نام خانوداگی انها با ت شروع میشود
+با استفاده از Subquery
+*/
+SELECT
+	o.EmployeeID, o.OrderID
+FROM dbo.Orders AS o
+	WHERE o.EmployeeID IN (SELECT EmployeeID FROM dbo.Employees WHERE lastname LIKE N'ت%');
+GO
+
+/*
+نمایش تمامی مشتریان تهران و اصفهان
+*/
+SELECT
+	CustomerID, City
+FROM dbo.Customers
+	WHERE City IN (N'تهران',N'اصفهان'); 
+GO
+
+/*
+لیست مشتریانی که سفارش ثبت نکرده اند.
+With JOIN
+*/
+SELECT
+	c.*
+FROM dbo.Customers AS c
+LEFT JOIN dbo.Orders AS o
+	ON o.CustomerID = c.CustomerID
+	WHERE o.OrderID IS NULL;
+GO
+/*
+شرط توی لفت جوین الکیه چون در اخر به خاطر جوین همه چیز رو میاره
+*/
+SELECT
+	c.*
+FROM dbo.Customers AS c
+LEFT JOIN dbo.Orders AS o
+	ON o.CustomerID = c.CustomerID
+	AND o.OrderID IS NULL;
+GO
+
+/*
+لیست مشتریانی که سفارش ثبت نکرده اند.
+With Self-Contained Multi-Valued Subquery
+*/
+
+SELECT 
+	*	
+FROM dbo.customers
+	WHERE CustomerID NOT IN (SELECT CustomerID FROM dbo.Orders);
+GO
+-- In this case, DISTINCT doesn't effect performance.
+SELECT 
+	*	
+FROM dbo.customers
+	WHERE CustomerID NOT IN (SELECT DISTINCT CustomerID FROM dbo.Orders);
+GO
+
+/*
+مشاهده مشخصات تمامی شرکت هایی که فقط در تایخ
+ 2016-05-05 درخواست سفارش نداشته اند 
+*/
+SELECT
+	c.CustomerID,
+	c.CompanyName
+FROM dbo.Customers AS c
+	WHERE c.CustomerID NOT IN(SELECT customerID FROM dbo.Orders WHERE OrderDate  = '2016-05-05');
+GO
+
