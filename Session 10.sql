755e6f2d5403196af77fffca4a392eb6720c16de
Erfan_akhavanrad <50219404+erfanakhavanrad@users.noreply.github.com>
2022-08-28 23:22:43 +0430

    Session 10 updated.

--- Session 10.sql
+++ Session 10.sql
@@ -152,3 +152,140 @@ GO
 
 SELECT * FROM dbo.All_Fields;
 GO
+
+-- Info about VIEW
+SP_HELPTEXT 'All_Fields'
+
+SELECT * FROM sys.sql_modules
+	WHERE object_id = object_id('All_Fields');
+GO
+
+-- دیگر VIEW در VIEW فراخوانی یک
+DROP VIEW IF EXISTS
+	dbo.Employees_OrderCount, dbo.Range_Employees_OrderCount;
+GO
+
+-- .ای که از طریق آن تعداد سفارشات هر کارمند شمارش می‌شود VIEW
+CREATE VIEW dbo.Employees_OrderCount
+AS
+	SELECT 
+		EmployeeID,
+		COUNT(OrderID) AS Num
+	FROM dbo.Orders
+	GROUP BY EmployeeID;
+GO
+
+-- Employees_OrderCount فراخوانی
+SELECT * FROM dbo.Employees_OrderCount;
+GO
+
+-- دیگری فراخوانی می‌شود VIEW ای که در آن VIEW
+CREATE VIEW dbo.Range_Employees_OrderCount
+AS
+	SELECT * FROM dbo.Employees_OrderCount
+		WHERE Num > 100;
+GO
+
+SELECT * FROM dbo.Range_Employees_OrderCount;
+GO
+
+DROP TABLE IF EXISTS dbo.SchemaBindingTbl;
+GO
+
+CREATE TABLE dbo.SchemaBindingTbl
+(
+	ID INT,
+	Family NVARCHAR(50)
+);
+GO
+
+INSERT INTO dbo.SchemaBindingTbl
+VALUES
+	(100,N'سعیدی'),
+	(200,N'کاردان'),
+	(300,N'شاکری');
+GO
+
+DROP VIEW IF EXISTS dbo.ViewBinding;
+GO
+
+CREATE VIEW ViewBinding
+AS
+	SELECT
+		ID, Family
+	FROM SchemaBindingTbl;
+GO
+
+SELECT * FROM dbo.ViewBinding;
+GO
+
+-- یکی از فیلدهای جدول Data Type تغییر 
+ALTER TABLE SchemaBindingTbl
+	ALTER COLUMN Family NVARCHAR(100);
+GO
+
+-- VIEW بر روی WITH SCHEMABINDING اضافه کردن تنظیمات
+ALTER VIEW ViewBinding WITH SCHEMABINDING
+AS
+	SELECT 
+		ID, Family
+	FROM dbo.SchemaBindingTbl;
+GO
+
+/*
+WITH SCHEMABINDING به‌دلیل وجود تنظیمات
+.داده نخواهد شد Family فیلد Data Type اجازه تغییر
+*/
+ALTER TABLE SchemaBindingTbl
+	ALTER COLUMN Family NVARCHAR(100);
+GO
+
+/*
+را تغییر داده و فیلد VIEW
+.را از آن حذف می‌کنیم Family
+*/
+ALTER VIEW ViewBinding WITH SCHEMABINDING
+AS
+	SELECT 
+		ID
+	FROM dbo.SchemaBindingTbl;
+GO
+
+SELECT * FROM ViewBinding;
+GO
+
+/*
+VIEW در Family با توجه به عدم وجود فیلد
+.تغییرات بدون هیچ‌گونه مشکلی در سمت جدول انجام خواهد شد
+*/
+ALTER TABLE SchemaBindingTbl
+	ALTER COLUMN Family NVARCHAR(100);
+GO
+
+/*
+کردن جدول داده نمی‌شود چرا که DROP اجازه
+.با جدول در ارتباط است WITH SCHEMABINDING از طریق VIEW
+*/
+DROP TABLE IF EXISTS SchemaBindingTbl;
+GO
+--------------------------------------------------------------------
+
+/*
+در حالت استفاده Object عدم نوشتن نام اسکیمای
+.موجب بروز خطا خواهد شد WITH SCHEMABINDING از تنظیمات 
+*/
+ALTER VIEW ViewBinding WITH SCHEMABINDING
+AS
+	SELECT 
+		ID
+	FROM SchemaBindingTbl; -- dbo.SchemaBindingTbl
+GO
+
+/*
+در حالت استفاده  SELECT * استفاده از
+.موجب بروز خطا خواهد شد WITH SCHEMABINDING از تنظیمات 
+*/
+ALTER VIEW ViewBinding WITH SCHEMABINDING
+AS
+	SELECT * FROM dbo.SchemaBindingTbl;
+GO
